a85bc59e13912535a27a28bf0607d711c0ccd0c9
e-kichi <nagayoshi1116@gmail.com>
2017-11-28 20:07:35 +0900

    ActionCableが接続するurlを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'ws://52.197.251.88/cable'
   config.action_cable.allowed_request_origins = [ 'http://52.197.251.88' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
